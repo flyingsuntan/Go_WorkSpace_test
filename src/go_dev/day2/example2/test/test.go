b2d5765e3929e25a96800aaf07dfb9da0aba72ee
flyingsuntan <40205922+flyingsuntan@users.noreply.github.com>
2018-11-28 10:16:46 +0800

    1

--- src/go_dev/day2/example2/test/test.go
+++ src/go_dev/day2/example2/test/test.go
@@ -2,6 +2,11 @@ package test
 
 import "fmt"
 
+var Name string = "this is in test package"
+var Age int = 1000
+
 func init() {
 	fmt.Println("This is a test")
+	fmt.Println("test.package.Name=", Name)
+	fmt.Println("test.package.Age=", Age)
 }
